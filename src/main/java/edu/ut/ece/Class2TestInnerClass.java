663e2d8be35534d2a2767f608a1f2a51990bc776
Kaiyuan Wang <kaiyuanw@utexas.edu>
2016-03-03 20:33:05 -0600

    Update project

--- src/main/java/edu/ut/ece/Class2TestInnerClass.java
+++ src/main/java/edu/ut/ece/Class2TestInnerClass.java
@@ -35,11 +35,7 @@ public class Class2TestInnerClass {
 		return s;
 	}
 	
-//	static class T {
-//		public static void m(int a) {}
-//	}
-//	
-//	public static void voidMethod(int a) {
-//		T.m(a);
-//	}
+	public static void voidMethod(int a) {
+		System.out.println(a);
+	}
 }
