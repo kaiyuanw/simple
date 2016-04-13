eaab02cfbd2be351f90fe23c4324e6ef3466646b
Kaiyuan Wang <kaiyuanw@utexas.edu>
2016-04-12 22:58:48 -0500

    Add python script

---
+++ jacoco.py
@@ -0,0 +1,28 @@
+__author__ = 'Kaiyuan_Wang'
+
+import sys
+
+def main(src, dst):
+    jacoco = open(src)
+    src_string = ''
+    for line in jacoco:
+        src_string += line
+    jacoco.close()
+    pom = open(dst, 'r')
+    dst_string = ''
+    for line in pom:
+        if '</project>' in line:
+            dst_string += src_string + '\n' + line
+            break
+        dst_string += line
+    pom = open(dst, 'r+')
+    pom.write(dst_string)
+    pom.truncate()
+    pom.close()
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 2:
+        print 'Usage: python jacoco.py src dst'
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    main(src, dst)
